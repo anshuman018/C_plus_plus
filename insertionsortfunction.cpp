f44c0203be7b39d201d05cb41fa64d8cd0abc52b
Anshuman <findmeansh@gmail.com>
2024-01-27 11:18:51 +0530

    tekebaaazi

--- insertionsortfunction.cpp
+++ insertionsortfunction.cpp
@@ -1,17 +1,41 @@
-void insertionSort(int arr[], int n) {
-    if (n <= 1) {
-        return;
+#include <iostream>
+using namespace std;
+void insertionsort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j;
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (current < arr[j])
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = current;
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    insertionsort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 
-    insertionSort(arr, n - 1);
-
-    int lastElement = arr[n - 1];
-    int j = n - 2;
-
-    while (j >= 0 && arr[j] > lastElement) {
-        arr[j + 1] = arr[j];
-        j--;
-    }
-
-    arr[j + 1] = lastElement;
+    return 0;
 }
