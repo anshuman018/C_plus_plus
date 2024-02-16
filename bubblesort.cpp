651b9a1b1bce79dd145503095729a0cce5a4d310
Anshuman <findmeansh@gmail.com>
2024-02-16 09:13:11 +0530

    with main funtion

---
+++ bubblesort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+void bubblesort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubblesort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
--- selection.cpp
+++ selection.cpp
@@ -1,35 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void selectionsort(int arr[],int n){
-    for(int i=0; i<n; i++){
+void selectionsort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         int min = arr[i];
         int minidx = i;
         int j;
-        for(j=i+1; j<n; j++){
-            if(arr[j]<min){
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < min)
+            {
                 min = arr[j];
                 minidx = j;
             }
         }
-        //swap;
+        // swap;
         int temp = arr[i];
         arr[i] = arr[minidx];
         arr[minidx] = temp;
     }
-
 }
 
-int main() {
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    selectionsort(arr,n);
+    selectionsort(arr, n);
 
-    for(int j = 0; j<n; j++){
-        cout<<arr[j]<<" ";
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << " ";
     }
     return 0;
 }
